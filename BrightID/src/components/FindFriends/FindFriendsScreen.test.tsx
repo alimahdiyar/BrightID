dfc3073717a29decf651b7ce1591d976816f55db
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-05-10 20:26:54 +0430

    just saving

---
+++ BrightID/src/components/FindFriends/FindFriendsScreen.test.tsx
@@ -0,0 +1,8 @@
+import { render } from 'react-dom';
+import FindFriendsScreen from './FindFriendsScreen';
+
+describe('Test channel data', () => {
+  test(`creates channel type`, async () => {
+    const screen = render(<FindFriendsScreen />);
+  });
+});
