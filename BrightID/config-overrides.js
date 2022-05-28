418dad9ed8889bba32c667d5b8e85f1b14ac3abc
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-05-28 21:17:31 +0430

    fix: some import problems in babel

--- BrightID/config-overrides.js
+++ BrightID/config-overrides.js
@@ -12,7 +12,7 @@ module.exports = module.exports = function override(config) {
   // Let's force our code to bundle using the same bundler react native does.
   config.plugins.push(new webpack.DefinePlugin({__DEV__: true}));
   config.module.rules.push({
-    test: /\.(js|tsx?)$/,
+    test: /\.(svg|js|tsx?)$/,
     /**
      * You can exclude the exclude property if you don't want to keep adding individual node_modules
      * just keep an eye on how it effects your build times, for this example it's negligible */
